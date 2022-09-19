5bc9e6ef4f97e74b9158db22793705be91f51341
+ <+>
2022-09-18 23:26:35 -0400

    commit tree ccc11f5632daae70a26b5727e9600fc6c0b57c0f

--- app/models/cheese.rb
+++ app/models/cheese.rb
@@ -3,5 +3,19 @@ class Cheese < ApplicationRecord
   def summary
     "#{name}: $#{price}"
   end
+  def show
+    cheese = Cheese.find_by(id: params[:id])
+    if cheese
+      render json: cheese, except: [:created_at, :updated_at], methods: [:summary]
+    else
+      # status: :not_found will produce a 404 status code
+      render json: { error: 'Cheese not found' }, status: :not_found
+    end
+  end
+
+  def index
+    cheeses = Cheese.all
+    render json: cheeses, except: [:created_at, :updated_at]
+  end
 
 end
